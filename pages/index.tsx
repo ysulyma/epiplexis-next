f4f1b787856e985d73f5a6bcd25758e20020f37c
Yuri Sulyma <yuri.sulyma@protonmail.com>
2023-11-30 09:34:22 -0500

    directory listing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,25 +3,84 @@ import {promises as fsp} from "fs";
 import type {GetStaticProps, InferGetStaticPropsType} from "next";
 import Link from "next/link";
 
-type Filetree = string[];
+type Dir = {
+  name: string;
+  children: (string | Dir)[];
+};
 
 export const getStaticProps = (async () => {
-  const files = await fsp.readdir("pages");
-  return {props: {files}};
+  const dir = await buildTree("./pages");
+  return {props: {dir}};
 }) satisfies GetStaticProps<{
-  files: Filetree;
+  dir: Dir;
 }>;
 
 export default function Page({
-  files,
+  dir,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <ol>
-      {files.map((file) => (
-        <li key={file}>
-          <Link href={`/${file}`}>{file}</Link>
-        </li>
-      ))}
-    </ol>
+    <main className="p-4 text-2xl leading-normal">
+      <h2 className="my-2 text-3xl">Content</h2>
+      <ol className="font-mono">
+        {dir.children.map((item) =>
+          typeof item === "string" ? null : <Tree key={item.name} dir={item} />,
+        )}
+      </ol>
+    </main>
   );
+}
+
+function Tree({dir}: {dir: Dir}) {
+  return (
+    <li>
+      {formatPath(dir.name) + "/"}
+      {dir.children.length > 0 && (
+        <ol className="ml-8">
+          {dir.children.map((item) => {
+            if (typeof item === "string") {
+              return (
+                <li key={item}>
+                  <Link className="text-blue-600" href={href(item)}>
+                    {formatPath(item)}
+                  </Link>
+                </li>
+              );
+            } else {
+              return <Tree key={item.name} dir={item} />;
+            }
+          })}
+        </ol>
+      )}
+    </li>
+  );
+}
+
+async function buildTree(dirname: string): Promise<Dir> {
+  const files = await fsp.readdir(dirname);
+  const dir: Dir = {
+    name: dirname,
+    children: [],
+  };
+
+  for (const file of files) {
+    const filename = `${dirname}/${file}`;
+
+    const stat = await fsp.stat(filename);
+
+    if (stat.isDirectory()) {
+      dir.children.push(await buildTree(filename));
+    } else {
+      dir.children.push(filename);
+    }
+  }
+
+  return dir;
+}
+
+function formatPath(path: string) {
+  return href(path).split("/").at(-1)!;
+}
+
+function href(path: string) {
+  return path.replace("./pages", "").replace(/\.tsx$/, "");
 }
