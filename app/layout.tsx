348ccd82e985a9edecb3608440e183f6fe903d6f
Yuri Sulyma <yuri.sulyma@protonmail.com>
2024-01-31 19:59:39 -0500

    dark mode in app dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,23 @@
-import "../pages/global.css";
+"use client";
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+import {useSearchParams} from "next/navigation";
+
+import "../pages/global.css";
+import {syncDarkMode} from "@/lib/dark-mode";
+
+export default function RootLayout({
+  children,
+  ...props
+}: {
+  children: React.ReactNode;
+}) {
+  const searchParams = useSearchParams();
+  const isDark = searchParams?.has("dark") ?? false;
+  syncDarkMode();
+
   return (
-    <html lang="en">
-      <body className="dark:text-white">{children}</body>
+    <html className={isDark ? "dark" : undefined} lang="en">
+      <body className="dark:bg-stone-800 dark:text-white">{children}</body>
     </html>
   );
 }
