3ff22e18ebe0b311b5775a07e86ea152382adc97
Yuri Sulyma <yuri.sulyma@protonmail.com>
2024-06-19 02:48:45 -0400

    polygon exercise

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,37 @@
+import classNames from "classnames";
 import {type ClassValue, clsx} from "clsx";
-import {twMerge} from "tailwind-merge";
+import {Children, createElement, forwardRef} from "react";
+import {ClassNameValue, twMerge} from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+/**
+ * Get a styled component from a template. Handles forwarding refs.
+ */
+export function brand<TagName extends keyof JSX.IntrinsicElements>(
+  displayName: string,
+  template: React.ReactNode,
+  mergeClassNames: (...args: ClassNameValue[]) => string = classNames,
+) {
+  type Ref = React.ComponentRef<TagName>;
+  type Props = JSX.IntrinsicElements[TagName];
+  const node = Children.only(template) as React.ReactElement<Props, TagName>;
+  const {className: templateClassName, ...templateProps} = node.props as ({className?: string;} & typeof node.props);
+
+  const component = function (
+    {className, ...props}: Props & {className?: string},
+    ref: React.ForwardedRef<Ref>,
+  ) {
+    return createElement(node.type, {
+      className: mergeClassNames(templateClassName, className),
+      ...templateProps,
+      ...props,
+      ref,
+    });
+  };
+  component.displayName = displayName;
+
+  return forwardRef(component);
 }
